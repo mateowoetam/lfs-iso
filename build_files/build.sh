f5f66f6c37cbc557945ac65ef2769c7ab20685ef
mateowoetam <mateo@orlaineta.net>
2026-07-04 21:29:40 -0600

    fixed autologin overrides in build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,41 +1,60 @@
 #!/bin/sh
 set -ouex pipefail
+
 LFS_VERSION="13.0"
-if [ -d "/ctx/system_files" ];then
-cp -avf "/ctx/system_files"/. /
+
+if [ -d "/ctx/system_files" ]; then
+    cp -avf "/ctx/system_files"/. /
 fi
+
+# Install Core Build Tools & GUI Apps
 dnf5 -y install \
-bash binutils bison coreutils diffutils findutils \
-gawk gcc gcc-c++ grep gzip m4 make patch perl \
-python3 sed tar texinfo xz xz-devel wget curl glibc-devel \
-gparted parted util-linux \
-firefox leafpad xterm opendoas sddm
+    bash binutils bison coreutils diffutils findutils \
+    gawk gcc gcc-c++ grep gzip m4 make patch perl \
+    python3 sed tar texinfo xz xz-devel wget curl glibc-devel \
+    gparted parted util-linux \
+    firefox leafpad xterm opendoas sddm
+
 dnf5 -y groupinstall "KDE Plasma Desktop"
+
+# Setup User and Permissions
 groupadd -f wheel
 groupadd lfs
 useradd -s /bin/bash -g lfs -G wheel -m lfs
 passwd -d lfs
-echo "lfs ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
-echo "permit nopass :wheel" >/etc/doas.conf
+
+echo "lfs ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo "permit nopass :wheel" > /etc/doas.conf
 chown root:root /etc/doas.conf
 chmod 0600 /etc/doas.conf
+
+# Configure SDDM Autologin
 mkdir -p /etc/sddm.conf.d
-cat <<EOF >/etc/sddm.conf.d/autologin.conf
+cat <<EOF > /etc/sddm.conf.d/autologin.conf
 [Autologin]
 User=lfs
 Session=plasma
 EOF
+
 systemctl enable sddm
 systemctl set-default graphical.target
+
+# PAM Fixes for Passwordless Login/Autologin
 sed -i 's/nullok_secure/nullok/' /etc/pam.d/system-auth
 sed -i 's/nullok_secure/nullok/' /etc/pam.d/password-auth
 sed -i 's/include system-auth/auth required pam_permit.so\ninclude system-auth/' /etc/pam.d/sddm-autologin
+
+# Download LFS Documentation
 DESKTOP_DIR="/home/lfs/Desktop"
 mkdir -p "$DESKTOP_DIR"
 BASE_URL="https://www.linuxfromscratch.org/lfs/downloads/stable-systemd"
+
 wget -P "$DESKTOP_DIR" "$BASE_URL/LFS-BOOK-$LFS_VERSION-NOCHUNKS.html"
 wget -P "$DESKTOP_DIR" "$BASE_URL/LFS-BOOK-$LFS_VERSION-SYSD.pdf"
 wget -P "$DESKTOP_DIR" "$BASE_URL/LFS-BOOK-$LFS_VERSION.tar.xz"
+
 tar -xf "$DESKTOP_DIR/LFS-BOOK-$LFS_VERSION.tar.xz" -C "$DESKTOP_DIR"
+
+# Permissions Cleanup & Package Cache Clear
 chown -R lfs:lfs /home/lfs
 dnf5 clean all

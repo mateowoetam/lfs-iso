457f521d149d5af50a8bba764ca1a69b47ec8e0c
mateowoetam <mateo@orlaineta.net>
2026-07-18 21:58:32 -0600

    Updated Containerfile image and build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,49 +1,33 @@
 #!/bin/sh
 set -ouex pipefail
-
 LFS_VERSION="13.0"
-
-if [ -d "/ctx/system_files" ]; then
-    cp -avf "/ctx/system_files"/. /
+if [ -d "/ctx/system_files" ];then
+cp -avf "/ctx/system_files"/. /
 fi
-
-# Install Core Build Tools & GUI Apps
-dnf -y install \
-   bash binutils bison busybox  coreutils curl dash \
-   diffutils distrobox findutils firefox fish  gawk gcc gcc-c++ \
-   git glibc-devel gnupg gparted grep gzip  ksh leafpad m4 \
-   make nushell opendoas parted patch perl  python3 sed tar \
-   texinfo util-linux wget xonsh xterm \
-   xz  xz-devel zsh
+dnf -y install --setopt=install_weak_deps=False \
+bash binutils bison busybox coreutils curl dash \
+diffutils distrobox findutils firefox fish gawk gcc gcc-c++ \
+git glibc-devel gnupg gparted grep gzip ksh leafpad m4 \
+make nushell opendoas parted patch perl python3 sed tar \
+texinfo util-linux wget xonsh xterm \
+xz xz-devel zsh
 dnf remove -y \
-   fcitx5 filelight htop kcharselect kde-connect \
-   kdebugsettings  kfind krfb kwalletmanager kwrite nvtop
-
-#dnf install -y @kde-desktop-environment
-
-# Setup User and Permissions
+fcitx5 filelight htop kcharselect kde-connect \
+kdebugsettings kfind krfb kwalletmanager kwrite nvtop
 groupadd -f wheel
 groupadd lfs
 useradd -s /bin/bash -g lfs -G wheel -m lfs
 passwd -d lfs
-
-echo "lfs ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-echo "permit nopass :wheel" > /etc/doas.conf
+echo "lfs ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
+echo "permit nopass :wheel" >/etc/doas.conf
 chown root:root /etc/doas.conf
 chmod 0600 /etc/doas.conf
-
-
-# Download LFS Documentation
 DESKTOP_DIR="/home/lfs/Desktop"
 mkdir -p "$DESKTOP_DIR"
 BASE_URL="https://www.linuxfromscratch.org/lfs/downloads/stable-systemd"
-
 wget -P "$DESKTOP_DIR" "$BASE_URL/LFS-BOOK-$LFS_VERSION-NOCHUNKS.html"
 wget -P "$DESKTOP_DIR" "$BASE_URL/LFS-BOOK-$LFS_VERSION-SYSD.pdf"
 wget -P "$DESKTOP_DIR" "$BASE_URL/LFS-BOOK-$LFS_VERSION.tar.xz"
-
 tar -xf "$DESKTOP_DIR/LFS-BOOK-$LFS_VERSION.tar.xz" -C "$DESKTOP_DIR"
-
-# Permissions Cleanup & Package Cache Clear
 chown -R lfs:lfs /home/lfs
 dnf clean all
